759e14bc81b8c4673dc99ae7531fa3995fd9145b
dan <vedantmartin@gmail.com>
2021-07-25 18:41:01 +0530

    figured out how to show no value messages

---
+++ script.js
@@ -0,0 +1,8 @@
+// const percent = Array.from(document.querySelectorAll('.perc'));
+
+
+// function output(e) {
+//     console.log(e)
+// }
+
+// percent.forEach(num => num.addEventListener('click', output));
